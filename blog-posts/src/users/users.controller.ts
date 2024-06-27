b86f53f8368d3b70a32dff3d17f8fa1b8e74702d
Hackim18 <khakim668@gmail.com>
2024-06-28 03:28:44 +0700

    feat: add register user

--- blog-posts/src/users/users.controller.ts
+++ blog-posts/src/users/users.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post('register')
+  async register(
+    @Body('name') name: string,
+    @Body('email') email: string,
+    @Body('password') password: string,
+  ) {
+    const user = await this.usersService.findByEmail(email);
+    if (user) {
+      return {
+        message: 'User already exists',
+      };
+    }
+    return this.usersService.createUser(name, email, password);
+  }
+}
--- blog-posts/src/users/users.service.ts
+++ blog-posts/src/users/users.service.ts
@@ -1,4 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { User } from './user.model';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectModel(User)
+    private userModel: typeof User,
+  ) {}
+
+  async createUser(
+    name: string,
+    email: string,
+    password: string,
+  ): Promise<Omit<User, 'password'>> {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await this.userModel.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    return this.excludePassword(user);
+  }
+
+  async findByEmail(email: string): Promise<User> {
+    return this.userModel.findOne({ where: { email } });
+  }
+
+  async findById(id: number): Promise<User> {
+    return this.userModel.findByPk(id);
+  }
+
+  private excludePassword(user: User): Omit<User, 'password'> {
+    const { password, ...userWithoutPassword } = user.toJSON();
+    return userWithoutPassword;
+  }
+}
