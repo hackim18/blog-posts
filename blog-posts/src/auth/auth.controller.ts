b5147f8e47ab6e0d8c114f8da9c2feec50994d35
Hackim18 <khakim668@gmail.com>
2024-06-28 03:41:01 +0700

    feat: add login authentication

--- blog-posts/src/auth/auth.controller.ts
+++ blog-posts/src/auth/auth.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UseGuards, Request } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './local-auth.guard';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  async login(@Request() req) {
+    if (!req.user) {
+      return {
+        message: 'Invalid credentials',
+      };
+    }
+    return this.authService.login(req.user.dataValues);
+  }
+}
