fc554426de910e4a26b3e077ca22eefe367fd6b2
Hackim18 <khakim668@gmail.com>
2024-06-28 03:16:16 +0700

    feat: add nest g controller, module, & service auth

--- blog-posts/src/app.module.ts
+++ blog-posts/src/app.module.ts
@@ -5,9 +5,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [SequelizeModule.forRoot(sequelizeConfig), UsersModule, PostsModule],
+  imports: [SequelizeModule.forRoot(sequelizeConfig), UsersModule, PostsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
