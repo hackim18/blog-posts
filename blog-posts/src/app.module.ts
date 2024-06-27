be759475df86c7e15bd8d172fbdb5d413373bb98
Hackim18 <khakim668@gmail.com>
2024-06-28 03:11:59 +0700

    feat: add config database

--- blog-posts/src/app.module.ts
+++ blog-posts/src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { sequelizeConfig } from './config/sequelize.config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [SequelizeModule.forRoot(sequelizeConfig)],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
